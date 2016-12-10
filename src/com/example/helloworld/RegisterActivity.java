c3c52bbf093e8962cf83b90839bac785715f316f
Administrator <Administrator@7A304-0025>
2016-12-10 14:57:27 +0800

    加密以及照片上传

--- src/com/example/helloworld/RegisterActivity.java
+++ src/com/example/helloworld/RegisterActivity.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import com.example.helloworld.fragments.inputcells.SimpleTextInputCellFragment;
 
 import android.app.Activity;
+import com.example.helloworld.fragments.inputcells.PictureInputCellFragment;
+import com.example.helloworld.MD5;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
@@ -27,98 +30,108 @@ public class RegisterActivity extends Activity {
 	SimpleTextInputCellFragment fragInputCellPasswordRepeat;
 	SimpleTextInputCellFragment fragInputCellEmail;
 	ProgressDialog progressDialog;
-	private String TAG="ggg";
-	
+	PictureInputCellFragment fragInputCellPicture;
+	private String TAG = "ggg";
+	private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_register);
-		
-		fragInputCellAccount = (SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_account);
-		fragInputCellName = (SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_name);
-		fragInputCellEmail=(SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_email);
-		fragInputCellPassword = (SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_password);
-		fragInputCellPasswordRepeat = (SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_password_repeat);
+
+		fragInputCellAccount = (SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_account);
+		fragInputCellName = (SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_name);
+		fragInputCellEmail = (SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_email);
+		fragInputCellPassword = (SimpleTextInputCellFragment) getFragmentManager()
+				.findFragmentById(R.id.input_password);
+		fragInputCellPasswordRepeat = (SimpleTextInputCellFragment) getFragmentManager()
+				.findFragmentById(R.id.input_password_repeat);
+		fragInputCellPicture = (PictureInputCellFragment) getFragmentManager().findFragmentById(R.id.input_picture);
 		findViewById(R.id.btn_submit).setOnClickListener(new View.OnClickListener() {
-	
-	@Override
-	public void onClick(View v) {
-		onRegister();
-	}
-});
-		progressDialog=new ProgressDialog(this);
+
+			@Override
+			public void onClick(View v) {
+				onRegister();
+			}
+		});
+		progressDialog = new ProgressDialog(this);
 		progressDialog.setMessage("请稍后");
 		progressDialog.setCanceledOnTouchOutside(false);
 		progressDialog.setCancelable(false);
-}
+	}
 
-private void onRegister() {
-String password=fragInputCellPassword.getText();
-String passwordRepeat=fragInputCellPasswordRepeat.getText();
-if(!password.equals(passwordRepeat)){
-	Toast.makeText(this, "两次输入的密码不一致", Toast.LENGTH_SHORT).show();
-	return;
-}
-String account=fragInputCellAccount.getText();
-String email=fragInputCellEmail.getText();
-String name=fragInputCellName.getText();
+	private void onRegister() {
+		String password = fragInputCellPassword.getText();
+		String passwordRepeat = fragInputCellPasswordRepeat.getText();
+		if (!password.equals(passwordRepeat)) {
+			Toast.makeText(this, "两次输入的密码不一致", Toast.LENGTH_SHORT).show();
+			return;
+		}
 
-	progressDialog.show();
+		String account = fragInputCellAccount.getText();
+		String email = fragInputCellEmail.getText();
+		String name = fragInputCellName.getText();
 
-OkHttpClient client=new OkHttpClient();
-RequestBody body=new MultipartBody.Builder().addFormDataPart("account", account)
-		.addFormDataPart("name", name)
-		.addFormDataPart("passwordHash", password)
-		.addFormDataPart("email", email).build();
-Request request = new Request.Builder().method("GET", null).
-		post(body)
-		.url("http://172.27.0.54:8080/membercenter/api/register")
-		.build();
-//异步发起请求
-client.newCall(request).enqueue(new Callback() {
-	
-	@Override
-	public void onResponse(final Call arg0, final Response arg1) throws IOException {
-		runOnUiThread(new Runnable() {
-			
+		progressDialog.show();
+
+		OkHttpClient client = new OkHttpClient();
+
+		MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM)
+				.addFormDataPart("account", account).addFormDataPart("name", name)
+				.addFormDataPart("passwordHash", MD5.getMD5(password)).addFormDataPart("email", email);
+		byte[] b = fragInputCellPicture.getPngData();
+		if (b != null) {
+			builder.addFormDataPart("avatar", "avatar",
+					RequestBody.create(MEDIA_TYPE_PNG, fragInputCellPicture.getPngData()));
+		}
+		MultipartBody body = builder.build();
+
+		Request request = new Request.Builder().method("GET", null).post(body)
+				.url("http://172.27.0.54:8080/membercenter/api/register").build();
+		// 异步发起请求
+		client.newCall(request).enqueue(new Callback() {
+
 			@Override
-			public void run() {
-				RegisterActivity.this.onResponse(arg0, arg1);
+			public void onResponse(final Call arg0, final Response arg1) throws IOException {
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						RegisterActivity.this.onResponse(arg0, arg1);
+					}
+				});
+			}
+
+			@Override
+			public void onFailure(Call arg0, IOException arg1) {
+				RegisterActivity.this.onFailure(arg0, arg1);
 			}
 		});
 	}
-	
-	@Override
-	public void onFailure(Call arg0, IOException arg1) {
-		RegisterActivity.this.onFailure(arg0, arg1);
-	}
-});
-}
 
-	private void onResponse(Call call, Response response){
+	private void onResponse(Call call, Response response) {
 		progressDialog.dismiss();
-	try {
-		new AlertDialog.Builder(this).setTitle("请求成功").setMessage(response.body().string())
-		.setNegativeButton("确定", null)
-		.show();
-	} catch (IOException e) {
-		e.printStackTrace();
-		Log.e(TAG, e.getMessage());
-		onFailure(call, e);
-	}
+		try {
+			new AlertDialog.Builder(this).setTitle("请求成功").setMessage(response.body().string())
+					.setNegativeButton("确定", null).show();
+		} catch (IOException e) {
+			e.printStackTrace();
+			Log.e(TAG, e.getMessage());
+			onFailure(call, e);
+		}
 	}
 
-	private void onFailure(Call call, Exception e){
+	private void onFailure(Call call, Exception e) {
 		progressDialog.dismiss();
-		Log.e(TAG, "异常"+e.getMessage());
+		Log.e(TAG, "异常" + e.getMessage());
 		new AlertDialog.Builder(this).setTitle("请求失败").setMessage(e.getLocalizedMessage()).show();
 	}
-	
+
 	@Override
-	protected void onResume(){
+	protected void onResume() {
 		super.onResume();
-		
+
 		fragInputCellAccount.setLableText("用户名");
 		fragInputCellAccount.setHintText("输入用户名");
 		fragInputCellName.setLableText("昵称");
--- src/com/example/helloworld/fragments/inputcells/PictureInputCellFragment.java
+++ src/com/example/helloworld/fragments/inputcells/PictureInputCellFragment.java
@@ -8,6 +8,7 @@ import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
+import java.io.ByteArrayOutputStream;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -22,22 +23,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class PictureInputCellFragment extends Fragment {
-	final int REQUESTCODE_CAMERA=1;
-	final int REQUESTCODE_ALBUM=2;
-	
+	final int REQUESTCODE_CAMERA = 1;
+	final int REQUESTCODE_ALBUM = 2;
+
 	ImageView imageView;
 	TextView labelText;
 	TextView hintText;
-	
+	private byte[] pngData;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_inputcell_picture,container);
-		
-		
-		imageView = (ImageView)view.findViewById(R.id.image);
-		labelText = (TextView)view.findViewById(R.id.label);
-		hintText = (TextView)view.findViewById(R.id.hint);
-		
+		View view = inflater.inflate(R.layout.fragment_inputcell_picture, container);
+
+		imageView = (ImageView) view.findViewById(R.id.image);
+		labelText = (TextView) view.findViewById(R.id.label);
+		hintText = (TextView) view.findViewById(R.id.hint);
+
 		imageView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -46,72 +47,82 @@ public class PictureInputCellFragment extends Fragment {
 		});
 		return view;
 	}
-	
-	void onImageViewClicked(){
-		String[]items={
-				"≈ƒ’’",
-				"œ‡≤·"
-		};
-		
-		new AlertDialog.Builder(getActivity())
-		.setTitle(labelText.getText())
-		.setItems(items, new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				switch(which){
-					case 0:
-						takephoto();
-						break;
-						
-					case 1:
-						pickFromAlbum();
-						break;
-						
-					default:
-						break;
-					
-				}
-				
-			}
-		})
-		
-		.show();
-}
-	void takephoto(){
-		Intent itnt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		startActivityForResult(itnt,REQUESTCODE_CAMERA);
+
+	void onImageViewClicked() {
+		String[] items = { "≈ƒ’’", "œ‡≤·" };
+
+		new AlertDialog.Builder(getActivity()).setTitle(labelText.getText())
+				.setItems(items, new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						switch (which) {
+						case 0:
+							takephoto();
+							break;
+
+						case 1:
+							pickFromAlbum();
+							break;
+
+						default:
+							break;
+
+						}
+
+					}
+				})
+
+				.show();
 	}
-	
-	void pickFromAlbum(){
+
+	void takephoto() {
+		Intent itnt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(itnt, REQUESTCODE_CAMERA);
+	}
+
+	private void setPngData(Bitmap bmp) {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+		pngData = stream.toByteArray();
+	}
+
+	public byte[] getPngData() {
+		return pngData;
+	}
+
+	void pickFromAlbum() {
 		Intent itnt = new Intent(Intent.ACTION_PICK);
 		itnt.setType("image/*");
-		startActivityForResult(itnt,REQUESTCODE_ALBUM);
+		startActivityForResult(itnt, REQUESTCODE_ALBUM);
 	}
-		
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(requestCode==Activity.RESULT_CANCELED)return;
-		if(requestCode==REQUESTCODE_CAMERA){
-			Bitmap bmp=(Bitmap)data.getExtras().get("data");
+		if (requestCode == Activity.RESULT_CANCELED)
+			return;
+		if (requestCode == REQUESTCODE_CAMERA) {
+			Bitmap bmp = (Bitmap) data.getExtras().get("data");
+			setPngData(bmp);
 			imageView.setImageBitmap(bmp);
-		}else if(requestCode==REQUESTCODE_ALBUM){
+		} else if (requestCode == REQUESTCODE_ALBUM) {
 			Bitmap bmp;
-			try{
-				bmp=MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),data.getData());
+			try {
+				bmp = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+				setPngData(bmp);
 				imageView.setImageBitmap(bmp);
-			}catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	public void setLabelText(String labelText){
+
+	public void setLabelText(String labelText) {
 		this.labelText.setText(labelText);
 	}
-	
+
 	public void setHintText(String hintText) {
 		this.hintText.setText(hintText);
 	}
-	
-	}
+
+}
